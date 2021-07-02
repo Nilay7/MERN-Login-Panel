838179f1b61ffce4afaeacf2604e2d4d753fc0a0
Nilay <patelnilay33@gmail.com>
2021-07-02 16:57:55 -0400

    added signup and login routes

--- API/index.js
+++ API/index.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ type: 'application/json' }));
 
 require('./routes')(app);
 
--- API/routes/user.route.js
+++ API/routes/user.route.js
@@ -1,7 +1,51 @@
-const express = require('express');
 const router = require('express').Router();
+const {
+    check
+} = require('express-validator');
 const userController = require('../controllers/user.controller');
 
-router.get('/test', userController.test);
+router.post('/signup',
+    check('email')
+    .isEmail()
+    .withMessage('enter valid email address'),
+    check('username')
+    .isLength({
+        min: 6
+    })
+    .withMessage('must be at least 6 chars long'),
+    check('password')
+    .isLength({
+        min: 6
+    })
+    .withMessage('must be at least 6 chars long'),
+    check('firstname')
+    .isLength({
+        min: 1
+    })
+    .withMessage('please enter First Name'),
+    check('lastname')
+    .isLength({
+        min: 1
+    })
+    .withMessage('please enter Last Name'),
+    check('phonenumber')
+    .isLength({
+        min: 10
+    })
+    .withMessage('please enter valid number'),
+    userController.register);
+
+router.post('/login',
+    check('username')
+    .isLength({
+        min: 6
+    })
+    .withMessage('enter valid username'),
+    check('password')
+    .isLength({
+        min: 6
+    })
+    .withMessage('enter valid password'),
+    userController.login);
 
 module.exports = router;
