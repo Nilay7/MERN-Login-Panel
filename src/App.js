368be0e3ea0c8bf56eaa78aa17805de3fdcfe027
Nilay <patelnilay33@gmail.com>
2021-07-06 15:21:48 -0400

    added login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import Login from './components/Login/Login';
+import useToken from './hooks/useToken';
 
 function App() {
+  const { token, setToken } = useToken();
+
+  console.log('token',token)
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Landing Page</h1>
+      <Router>
+        <Switch>
+          <Route path='/dashboard'>
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
